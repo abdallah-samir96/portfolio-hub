c6d24fc481838393f7b3c58e48b12871a999dc7e
Abdallah Samir <abdallah01097261247@gmail.com>
2026-06-29 15:30:59 +0300

    adding changes in nav bar

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,9 @@
+import '../styles/About.css'
+
 function About() {
     return (
         <section className="mb-5">
-            <h2 className="section-title">About Me</h2>
+            <h2 className="section-title"><span className="section-title-info">About Me</span></h2>
             <div className="card shadow border-0 p-4">
                 Software Engineer experienced in Java, Kotlin, Spring Boot, Kafka,
                 React, Kubernetes and distributed systems with experience in healthcare
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,15 @@
-import {useDispatch, useSelector} from 'react-redux';
-import {toggleTheme} from '../store';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleTheme } from '../store';
 
 function Nav() {
-    const {dark} = useSelector( s => s.profile);
+    const { dark } = useSelector(s => s.profile);
     const dispatch = useDispatch();
     return (
         <nav className="navbar navbar-expand-lg shadow-sm sticky-top bg-body">
-            <div className="container">
-                <a className="navbar-brand fw-bold" href="/">Abdallah Samir</a>
+            <div className="container" style={{maxHeight: '30px'}}>
+                <a className="navbar-brand fw-bold" href="/">
+                    <i className="bi bi-code-slash" style = {{fontSize: '30px'}}></i>
+                </a>
                 <button
                     className="btn btn-outline-primary"
                     onClick={() => dispatch(toggleTheme())}>
