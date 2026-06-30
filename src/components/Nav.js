e5ab7b05bc12b53674ce32d64599ab605ae27800
Abdallah Samir <abdallah01097261247@gmail.com>
2026-06-30 14:24:52 +0300

    adding changes to navigation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,41 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleTheme } from '../store';
+import { useState, useEffect } from 'react';
 
 function Nav() {
     const { dark } = useSelector(s => s.profile);
     const dispatch = useDispatch();
+
+    const [time, setTime] = useState(
+    new Date().toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+    })
+    );
+
+    useEffect(() => {
+    const interval = setInterval(() => {
+        setTime(
+        new Date().toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit"
+        })
+        );
+    }, 1000);
+
+    return () => clearInterval(interval);
+    }, []);
+
     return (
         <nav className="navbar navbar-expand-lg shadow-sm sticky-top bg-body">
             <div className="container">
                 <a className="navbar-brand fw-bold" href="/">
                     <i className="bi bi-code-slash" style = {{fontSize: '30px'}}></i>
                 </a>
-                <button
-                    className="btn btn-outline-primary"
-                    onClick={() => dispatch(toggleTheme())}>
-                    <i className={`bi ${dark ? 'bi-sun' : 'bi-moon'}`}></i>
+                <button className="btn btn-outline-primary">
+                   <span className="fw-semibold">{time}</span>
                 </button>
             </div>
         </nav>
